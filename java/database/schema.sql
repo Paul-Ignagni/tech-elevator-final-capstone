bd38060e0c8c537a135d935a945ce02fad1a8d9d
Paul Ignagni <icerice965@gmail.com>
2023-08-07 13:35:57 -0400

    Schema.sql

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,69 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE comic_info (
+	serial_number INT NOT NULL UNIQUE,
+	author varchar(100) NOT NULL,
+	publisher varchar(100) NOT NULL,
+	series varchar(100),
+	genre varchar(50),
+	release_date DATE,
+	CONSTRAINT PK_serial PRIMARY KEY (serial_number)
+);
+
+CREATE TABLE character (
+	character_id SERIAL,
+	name varchar(100) NOT NULL,
+	CONSTRAINT PK_character PRIMARY KEY (character_id)
+);
+
+CREATE TABLE character_comic_info (
+	serial_number INT NOT NULL UNIQUE,
+	character_id INT NOT NULL UNIQUE,
+	CONSTRAINT FK_serial FOREIGN KEY (serial_number) REFERENCES comic_info(serial_number),
+	CONSTRAINT FK_character FOREIGN KEY (character_id) REFERENCES character(character_id)
+);
+
+CREATE TABLE comic_book (
+	comic_id SERIAL,
+	serial_number INT NOT NULL,
+	owner INT NOT NULL,
+	current_holder INT NOT NULL,
+	reminder DATE,
+	condition INT NOT NULL,
+	CONSTRAINT PK_comic PRIMARY KEY (comic_id),
+	CONSTRAINT FK_serial FOREIGN KEY (serial_number) REFERENCES comic_info(serial_number),
+	CONSTRAINT FK_owner FOREIGN KEY (owner) REFERENCES users(user_id),
+	CONSTRAINT FK_holder FOREIGN KEY (current_holder) REFERENCES users(user_id)
+);
+
+CREATE TABLE collection (
+	collection_id SERIAL,
+	user_id INT NOT NULL,
+	collection_name varchar(100) NOT NULL,
+	isPublic BOOLEAN NOT NULL,
+	CONSTRAINT PK_collection PRIMARY KEY (collection_id),
+	CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE collection_comic_book (
+	collection_id INT NOT NULL,
+	comic_id INT NOT NULL,
+	CONSTRAINT FK_collection FOREIGN KEY (collection_id) REFERENCES collection(collection_id),
+	CONSTRAINT FK_comic FOREIGN KEY (comic_id) REFERENCES comic_book(comic_id)
+);
+
+CREATE TABLE users_comic_info (
+	entry_id INT NOT NULL UNIQUE,
+	user_id INT NOT NULL,
+	serial_number INT NOT NULL,
+	rating INT,
+	review varchar(500),
+	read_status INT NOT NULL,
+	CONSTRAINT PK_entry PRIMARY KEY (entry_id),
+	CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_serial FOREIGN KEY (serial_number) REFERENCES comic_info(serial_number)
+);
+	
+
 COMMIT TRANSACTION;
