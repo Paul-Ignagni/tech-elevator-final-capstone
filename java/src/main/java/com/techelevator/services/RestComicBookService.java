da846434013a54e551466bbf98087d07cc823ed6
Nathan Jamieson <sbgnjayy@gmail.com>
2023-08-08 12:55:15 -0400

    api call

---
+++ java/src/main/java/com/techelevator/services/RestComicBookService.java
@@ -0,0 +1,10 @@
+package com.techelevator.services;
+
+import org.springframework.web.client.RestTemplate;
+
+public class RestComicBookService implements ComicBookService {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private static final String API_BASE_URL = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c30470c10899a5edb018d295b1cfa611&hash=bc4a36e9d13462b9323713e1aaec0750";
+
+}
