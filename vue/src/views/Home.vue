926db5c091fdbeb7c936f9acf3b10a7f6f0af8e3
Nathan Jamieson <sbgnjayy@gmail.com>
2023-08-08 22:58:42 -0400

    adding comic book style font and displaying images better

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <h1>Comic Book Search</h1>
-    <div>
-      <input v-model="searchQuery" placeholder="Enter comic name" />
-      <button @click="searchComics">Search</button>
+    <h1 class="comic-heading">Comic Book Search</h1>
+    <div class="search-container">
+      <input v-model="searchQuery" placeholder="Enter comic name" class="search-input" />
+      <button @click="searchComics" class="search-button">Search</button>
     </div>
     <div class="search-results">
       <div v-for="comic in comics" :key="comic.id" class="comic-card">
-        <img v-if="comic.images && comic.images.length > 0" :src="comic.images[0].path + '.' + comic.images[0].extension" alt="Comic Cover" />
-        <h2>{{ comic.title }}</h2>
-        <p v-if="comic.description">{{ comic.description }}</p>
-        <p>Issue Number: {{ comic.issueNumber }}</p>
-        <p>Page Count: {{ comic.pageCount }}</p>
-        <div v-for="image in comic.images" :key="image.path">
-          <img :src="image.path + '.' + image.extension" alt="Comic Cover" />
+        <div class="comic-cover">
+          <img v-if="comic.images && comic.images.length > 0" :src="comic.images[0].path + '.' + comic.images[0].extension" alt="Comic Cover" />
+        </div>
+        <div class="comic-details">
+          <h2>{{ comic.title }}</h2>
+          <p v-if="comic.description">{{ comic.description }}</p>
+          <p>Issue Number: {{ comic.issueNumber }}</p>
+          <p>Page Count: {{ comic.pageCount }}</p>
         </div>
       </div>
     </div>
@@ -42,7 +43,30 @@ export default {
 </script>
 
 <style>
-/* Add your styles here */
+
+.home {
+  text-align: center;
+}
+
+.comic-heading {
+  font-family: 'Comic Sans MS', cursive; /* Use a comic book font here */
+  font-size: 28px;
+}
+
+.search-container {
+  margin-top: 20px;
+}
+
+.search-input {
+  padding: 8px;
+  font-size: 16px;
+}
+
+.search-button {
+  padding: 8px 16px;
+  font-size: 16px;
+}
+
 .comic-card {
   border: 1px solid #ccc;
   padding: 10px;
@@ -50,6 +74,7 @@ export default {
   display: inline-block;
   max-width: 200px;
 }
+
 .comic-card img {
   max-width: 100%;
   height: auto;
