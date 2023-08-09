e90e143395f261f1276c344a3aacc33905c4cd13
Nathan Jamieson <sbgnjayy@gmail.com>
2023-08-08 22:34:18 -0400

    search added to display apis

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,20 +1,57 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Comic Book Search</h1>
+    <div>
+      <input v-model="searchQuery" placeholder="Enter comic name" />
+      <button @click="searchComics">Search</button>
+    </div>
+    <div class="search-results">
+      <div v-for="comic in comics" :key="comic.id" class="comic-card">
+        <img v-if="comic.images && comic.images.length > 0" :src="comic.images[0].path + '.' + comic.images[0].extension" alt="Comic Cover" />
+        <h2>{{ comic.title }}</h2>
+        <p v-if="comic.description">{{ comic.description }}</p>
+        <p>Issue Number: {{ comic.issueNumber }}</p>
+        <p>Page Count: {{ comic.pageCount }}</p>
+        <div v-for="image in comic.images" :key="image.path">
+          <img :src="image.path + '.' + image.extension" alt="Comic Cover" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import comicService from "../services/ComicService.js"
+import comicService from "../services/ComicService.js";
+
 export default {
   name: "home",
-  created(){
-    comicService.search("venom").then ((response) => {
-      console.log(response)
-      console.log(response.data.data.results[0].images[0])
-    })
-
+  data() {
+    return {
+      searchQuery: "",
+      comics: []
+    };
+  },
+  methods: {
+    searchComics() {
+      comicService.search(this.searchQuery).then((response) => {
+        this.comics = response.data.data.results;
+      });
+    }
   }
 };
 </script>
+
+<style>
+/* Add your styles here */
+.comic-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  display: inline-block;
+  max-width: 200px;
+}
+.comic-card img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
